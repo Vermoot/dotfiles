d8201e73845c8bce77095cd1aafa236c2008d45d
Vermoot <vermoot@gmail.com>
2024-05-21 18:05:21 +0200

    astronvim/start v4 migration

---
+++ .config/astronvim_v4/lua/plugins/gruvbox.lua
@@ -0,0 +1,11 @@
+return { "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  config = true,
+  opts = {
+    italic = {
+      strings = false,
+      comments = true,
+      operators = false,
+    },
+  }
+}
